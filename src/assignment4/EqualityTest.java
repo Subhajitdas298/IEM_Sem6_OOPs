c578b4e08ce7549cbb2916c3ceffbe7b7685c7b0
Subhajitdas298 <subhajitdas298@live.com>
2018-03-21 00:40:21 +0530

    Added Assignment 4

---
+++ src/assignment4/EqualityTest.java
@@ -0,0 +1,26 @@
+package assignment4;
+
+import java.util.Scanner;
+
+public class EqualityTest {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the original no : ");
+        final int original = input.nextInt();
+        
+        while(true){
+            System.out.print("Try a no : ");
+            if(original == input.nextInt()){
+                System.out.println("It is the original no.");
+            }else{
+                System.out.println("It is not the original no.");
+            }
+            
+            System.out.print("Exit? (y/n) : ");
+            if(input.next().equals("y")) return;
+        }
+    }
+    
+}
---
+++ src/assignment4/SumOfDigits.java
@@ -0,0 +1,23 @@
+package assignment4;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a no : ");
+        long n = input.nextLong();
+        
+        int sum = 0;
+        
+        while(n != 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        
+        System.out.println("Sum of digits : " + sum);
+    }
+    
+}
